e33b3d4ca62e74231d3da7036fb0f969c456c855
Guillaume LARGET <glarget@lesechosleparisien.fr>
2021-03-15 07:37:51 +0100

    refactor(header): add content

--- src/views/Header/index.tsx
+++ src/views/Header/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import type { ReactElement } from 'react';
 
+import Button from '../../components/atoms/Button';
 import Div from '../../components/atoms/Div';
-import Logo from '../../components/atoms/Logo';
+import Title from '../../components/atoms/Title';
 
-/**
- * Renders the main logo and the navigation bar
- */
 export default function Header(): ReactElement {
   return (
-    <>
-      <Div alignItems="center" display="flex" padding={{ _: '15px 22px', md: '15px 150px' }}>
-        <Div display="flex" alignItems="center">
-          <Logo id="meilleureReprise" />
-        </Div>
+    <Div alignItems="center" display="flex" padding={{ _: '15px 22px', md: '15px 150px' }}>
+      <Div display="flex" alignItems="center">
+        <Title as="h1" fontFamily="montSerratSemiBold" fontSize="40px">
+          PUYO PUYO
+        </Title>
       </Div>
-    </>
+      <Div marginLeft="auto">
+        <Button marginLeft="15px" backgroundColor="green" color="white">
+          Règles du jeu
+        </Button>
+      </Div>
+    </Div>
   );
 }
